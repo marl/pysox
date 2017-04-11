91a532aa255a8ba35874f02f9249a38b389697fa
Page David <david991010@gmail.com>
2017-04-11 08:20:15 +0800

    fix noiseprof output check.

--- sox/transform.py
+++ sox/transform.py
@@ -1875,9 +1875,10 @@ class Transformer(object):
 
         '''
         if os.path.isdir(profile_path):
-            raise ValueError("profile_path {} is a directory, but should be a file")
+            raise ValueError("profile_path {} is a directory, but filename should be specified.")
         
-        if not os.access(os.path.dirname(profile_path), os.W_OK):
+        if (bool(os.path.dirname(profile_path)) and
+            not os.access(os.path.dirname(profile_path), os.W_OK)):
             raise IOError("profile_path {} is not writeable.".format(profile_path))
 
         effect_args = ['noiseprof', profile_path]
